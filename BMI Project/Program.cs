47ce6206161a63ec834af439007f5c24c63ec7a6
Axelskold93 <142592414+Axelskold93@users.noreply.github.com>
2023-10-02 12:41:16 +0200

    Revert "Changed and removed functions"
    This reverts commit 577a774f43ecbd7b052021365a1356efe942b6ae.

--- BMI Project/Program.cs
+++ BMI Project/Program.cs
@@ -8,7 +8,7 @@ namespace BMI_Project
 {
     public class Person
     {
-        
+        public string Name { get; set; }
         public double Weight { get; set; }
         public double Height { get; set; }
         public List<double> BMIS = new List<double>();
@@ -16,16 +16,18 @@ namespace BMI_Project
         public void SetPersonValues()
         {
             Console.Clear();
-            Console.WriteLine("Please enter weight in pounds:");
+            Console.WriteLine("Please enter name:");
+            Name = Console.ReadLine();
+            Console.WriteLine("Please enter weight:");
             Weight = double.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter height in inches:");
+            Console.WriteLine("Please enter height:");
             Height = double.Parse(Console.ReadLine());
-            people.Add(new Person { Weight = Weight, Height = Height});
+            people.Add(new Person { Name = Name, Weight = Weight, Height = Height});
         }
         public void CalculateBMI()
         {
             Console.Clear();
-            double currentBMI = Weight / (Height * Height) * 703;
+            double currentBMI = Weight / (Height * Height);
             Console.WriteLine($"Current BMI: {currentBMI:N1}.");
             BMIS.Add(currentBMI);
             Console.ReadKey();
